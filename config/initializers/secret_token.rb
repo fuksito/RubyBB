389ad70b74efcf44c89be3886ef7568bdc2f30ef
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-11-07 00:11:32 +0100

    rails new RubyBB with RailsWizard
    Recipes:
    ActiveRecord Devise Git HAML jQuery SASS Redis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyBB::Application.config.secret_token = '98a0073dd8bf01aa7c86f5c3afe73efe3ea865b1e3d39fec7708d594bee492f7796184272e73cf9bc9950201feb96f50f86b1d9424ae36fb9838506ea4f9926c'
