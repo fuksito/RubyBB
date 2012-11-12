1522705042795ffe0a33bace77fec4378e9b0e88
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-11-12 23:41:25 +0100

    Optimization: message always need user.name

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   belongs_to :forum, :counter_cache => true
-  has_many :messages
+  has_many :messages, :include => [:user]
   attr_accessible :name, :user_id, :forum_id
 end
