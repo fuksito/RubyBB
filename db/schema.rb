585095a6f751512a012d8463d740b3e093747c97
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-11-15 20:51:16 +0100

    Use active record session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121114171557) do
+ActiveRecord::Schema.define(:version => 20121115190156) do
 
   create_table "forums", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(:version => 20121114171557) do
 
   add_index "messages", ["topic_id"], :name => "index_messages_on_topic_id"
   add_index "messages", ["user_id"], :name => "index_messages_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "topics", :force => true do |t|
     t.string   "name"
