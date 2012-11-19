04a011e7c9c3ee9ebb84c0af1b0f113bcbe19cbf
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-11-19 17:19:57 +0100

    username always build a link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def username user, forum_id = nil
-    content_tag :span, :class => user.role(forum_id) do
+    link_to user, :class => user.role(forum_id) do
       user.name
     end
   end
