446f63fed47ad33930916c331bda933558449b38
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-12-02 12:46:56 +0100

    Fix user.notifications_count on notif update

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,10 @@ class Notification < ActiveRecord::Base
   belongs_to :user, :counter_cache => true
   belongs_to :message
   attr_accessible :user_id, :message_id
+
+  after_update :increment_notifications_counter
+
+  def increment_notifications_count
+    self.user.increment(:notifications_count)
+  end
 end
