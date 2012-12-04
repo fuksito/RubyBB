2537175447cb1ea47d627ce3c16aa0bfa4e588b4
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-12-04 17:29:26 +0100

    Does not fire notification to ourself

--- app/models/small_message.rb
+++ app/models/small_message.rb
@@ -9,7 +9,9 @@ class SmallMessage < ActiveRecord::Base
   after_save :fire_notifications
 
   def fire_notifications
-    Notification.find_or_create_by_user_id_and_message_id(self.message.user_id, self.message_id).touch
+    if self.message.user_id != self.user_id
+      Notification.find_or_create_by_user_id_and_message_id(self.message.user_id, self.message_id).touch
+    end
     Follow.not_by(self.user_id).where(:followable_id => self.message_id, :followable_type => 'Message').each do |f|
       Notification.find_or_create_by_user_id_and_message_id(f.user_id, self.message_id).touch
     end
