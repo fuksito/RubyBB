820be5fa490d8ecbab10486019346a7767f7f543
Julien Grillot <julien.grillot+rubybb@gmail.com>
2012-12-11 17:51:33 +0100

    WIP faye, fire notifications to update counter

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,8 +3,13 @@ class Notification < ActiveRecord::Base
   belongs_to :message
 
   after_update :increment_notifications_counter
+  after_save :fire_notification
 
   def increment_notifications_count
     self.user.increment(:notifications_count)
+  end
+
+  def fire_notification
+    Net::HTTP.post_form(URI.parse("http://localhost:9292/faye"), message: {channel: "/#{self.user_id}/notifications", data: self}.to_json)
   end
 end
